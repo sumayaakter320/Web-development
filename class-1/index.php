a6610b56f8622da0ff950a14e1196523dae7f42a
sumayaakter320 <sumaya55230@gmail.com>
2023-12-22 19:14:16 +0600

    class-1 & index.php file is added

---
+++ class-1/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Class - 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1 style="color: red;">
+        <?php
+            echo "Hello World!";
+        ?>
+    </h1>
+    
+</body>
+</html>
+
+<?php
+    echo "<h2>Hi everyone... </h2>";
+?>
+
+<?php
+    $name = "Rayhana Akter Sumaya";
+    $Name = "Santa";
+    echo $NamE;
+    echo $name;
+?>
